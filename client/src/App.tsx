0b670c90bce85b1873379ead359ba6c385fb3cdb
Vipul <007vipulrank1@gmail.com>
2023-01-26 23:34:54 +0530

    ADD: navbar with night mode

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { FC, ReactElement, useEffect } from 'react'
+import Navbar from './components/Navbar'
+import { Routes, Route } from "react-router-dom"
+import Home from './pages/Home'
 
-const App = () => {
+const App: FC = (): ReactElement => {
+  useEffect(() => {
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.add('light')
+    }
+  }, [])
+
+  const handleModeChange = (): void => {
+    if (localStorage.getItem('theme')) {
+      if (localStorage.getItem('theme') === 'light') {
+        document.documentElement.classList.remove('light');
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
+      } else {
+        document.documentElement.classList.remove('dark');
+        document.documentElement.classList.add('light');
+        localStorage.setItem('theme', 'light');
+      }
+    } else {
+      if (document.documentElement.classList.contains('dark')) {
+        document.documentElement.classList.remove('dark');
+        document.documentElement.classList.add('light');
+        localStorage.setItem('theme', 'light');
+      } else {
+        document.documentElement.classList.remove('light');
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
+      }
+    }
+  };
+
   return (
-    <div className="font-main text-base">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="font-main text-base w-full min-h-screen dark:bg-zinc-900 dark:text-white bg-white text-zinc-900 transition-all duration-300">
+      <div className="px-4 sm:px-8 md:px-12 xl:px-0 max-w-7xl mx-auto">
+        <Navbar handleModeChange={handleModeChange} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<>Cart</>} />
+          <Route path="*" element={<>404</>} />
+        </Routes>
+      </div>
     </div>
   )
 }
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,8 +1,57 @@
-import React from 'react'
+import React, { FC, ReactElement } from 'react'
+import { Link } from "react-router-dom"
+import { v4 as uuidv4 } from "uuid"
+import { INavbar } from './interfaces/INavbar'
 
-const Navbar = () => {
+const NAV_DATA = [
+  { label: "Products", route: "/products" },
+  { label: "Cart", route: "/cart" },
+]
+
+const Navbar: FC<INavbar> = (props): ReactElement => {
+  const { handleModeChange } = props
+
   return (
-    <div>Navbar</div>
+    <header className="flex items-center justify-between py-6">
+      <button className="md:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
+      </button>
+      <div className="flex items-center justify-start gap-6 lg:gap-12">
+        <h1 className="font-bold uppercase text-xl">
+          <Link to="/">🛍️ CommerCafe</Link>
+        </h1>
+        <ul className="hidden md:flex items-center justify-start gap-4 lg:gap-6">
+          {NAV_DATA.map((item) => {
+            return <li key={uuidv4()}>
+              <Link to={item.route}>{item.label}</Link>
+            </li>
+          })}
+        </ul>
+      </div>
+      <button className="md:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+        </svg>
+      </button>
+      <div className="hidden md:flex items-center justify-start gap-4">
+        <button onClick={handleModeChange}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 hidden dark:block">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
+          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 dark:hidden">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
+          </svg>
+        </button>
+        <button className="px-3 py-2">
+          Login
+        </button>
+        <button className="px-4 py-2 border-2 border-indigo-600 bg-indigo-600 text-white rounded-md">
+          Sign up
+        </button>
+      </div>
+    </header>
   )
 }
 
