049d391f4fc9314fd13883a0c0030b66d94f813b
Vipul <007vipulrank1@gmail.com>
2023-01-26 23:38:56 +0530

    REF: uuid package

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React, { FC, ReactElement } from 'react'
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { v4 as uuidv4 } from "uuid"
 import { INavbar } from './interfaces/INavbar'
 
@@ -10,6 +10,7 @@ const NAV_DATA = [
 
 const Navbar: FC<INavbar> = (props): ReactElement => {
   const { handleModeChange } = props
+  const navigate = useNavigate()
 
   return (
     <header className="flex items-center justify-between py-6">
@@ -30,7 +31,7 @@ const Navbar: FC<INavbar> = (props): ReactElement => {
           })}
         </ul>
       </div>
-      <button className="md:hidden">
+      <button className="md:hidden" onClick={() => navigate("/cart")}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
         </svg>
@@ -44,10 +45,10 @@ const Navbar: FC<INavbar> = (props): ReactElement => {
             <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
           </svg>
         </button>
-        <button className="px-3 py-2">
+        <button className="px-3 py-2" onClick={() => navigate("/login")}>
           Login
         </button>
-        <button className="px-4 py-2 border-2 border-indigo-600 bg-indigo-600 text-white rounded-md">
+        <button className="px-4 py-2 border-2 border-indigo-600 bg-indigo-600 text-white rounded-md" onClick={() => navigate("/register")}>
           Sign up
         </button>
       </div>
