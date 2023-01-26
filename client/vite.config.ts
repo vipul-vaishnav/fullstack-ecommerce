850f928b7abf8afc24ccf54274efd411d2f9016d
Vipul <007vipulrank1@gmail.com>
2023-01-27 00:19:38 +0530

    REF: env file

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const PORT = Number(process.env.CLIENT_PORT || 3000)
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: PORT
+    port: 3000
   }
 })
